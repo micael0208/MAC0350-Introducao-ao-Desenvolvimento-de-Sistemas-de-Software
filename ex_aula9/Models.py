f9dbe08bdd55262d11d8ca996978ffd7cebac6f6
Micael Vinicius Lira Prado <85125986+micael0208@users.noreply.github.com>
2026-04-07 10:07:26 -0300

    Criar Models.py

---
+++ ex_aula9/Models.py
@@ -0,0 +1,8 @@
+# Models.py
+
+from typing import Optional
+from sqlmodel import Field, SQLModel
+
+class Aluno(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    nome: str
